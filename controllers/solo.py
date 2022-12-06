8917acdb7dac6d449d3aeb720bb049c74b0aae42
aljanabim <ma970101@yahoo.se>
2022-12-06 21:53:29 +0100

    Working Frenet Example, fixed error calc for yaw error

--- controllers/solo.py
+++ controllers/solo.py
@@ -13,11 +13,27 @@ class SoloMPC(GenericMPC):
 
     def set_cost(self):
         self.cost = 0
+
+        n_sub = self.n_states - 1  # number of states ignore the yaw
         for i in range(self.N):
-            err = self.x[:, i] - self.x_ref[:, i]
-            self.cost += err.T @ self.Q @ err + self.u[:, i].T @ self.R @ self.u[:, i]
-        errN = self.x[:, self.N] - self.x_ref[:, self.N]
-        self.cost += errN.T @ self.Q @ errN
+            err = self.x[:n_sub, i] - self.x_ref[:n_sub, i]
+            self.cost += (
+                err.T @ self.Q[:n_sub, :n_sub] @ err
+                + self.u[:, i].T @ self.R @ self.u[:, i]
+            )
+            # Calculate the angle difference towards the smallest angle error
+            # from https://math.stackexchange.com/questions/341749/how-to-get-the-minimum-angle-between-two-crossing-lines
+            angle_err = ca.pi - ca.norm_2(
+                ca.norm_2(self.x[-1, i] - self.x_ref[-1, i]) - ca.pi
+            )
+            self.cost += angle_err**2 * self.Q[-1, -1]
+
+        errN = self.x[:n_sub, self.N] - self.x_ref[:n_sub, self.N]
+        self.cost += errN.T @ self.Q[:n_sub, :n_sub] @ errN
+        angle_err = ca.pi - ca.norm_2(
+            ca.norm_2(self.x[-1, self.N] - self.x_ref[-1, self.N]) - ca.pi
+        )
+        self.cost += angle_err**2 * self.Q[-1, -1]
 
     def get_ctrl(self, x0, x_ref, curvature, s_0_arc, phi_0_arc):
         self.opti.set_value(self.x0, x0)
--- solo.py
+++ solo.py
@@ -10,21 +10,23 @@ from utils.sim_util import (
     create_track,
     get_data_folder,
 )
-from utils.vis_util import VehicleData, COLORS, animate_trajectory
+from utils.vis_util import VehicleData, COLORS, animate_trajectory, plot_trajectory
 
 if __name__ == "__main__":
     lane_width = 0.5
 
-    track = create_track(0, 0, ca.pi / 4)
+    yaw0 = 0  # ca.pi
+    track = create_track(0, 0, yaw0)
 
-    x = ca.vertcat(0, lane_width / 2, 0, 0)
-    xub = ca.vertcat(track.length, lane_width, 1.5, 2 * ca.pi)
+    x = ca.vertcat(0, -lane_width / 2, 0, yaw0)
+    # +0.1 on s to allow reaching the target
+    xub = ca.vertcat(track.length + 0.1, lane_width, 1.5, ca.inf)
     uub = ca.vertcat(1, ca.pi / 5)
     model = SoloFrenetModel(x)
     mpc = SoloMPC(
         model,
         Q=ca.diag((1, 300, 200, 20)),
-        R=ca.diag((5, 1)),
+        R=ca.diag((5, 4)),
         xlb=-xub,
         xub=xub,
         ulb=-uub,
@@ -33,19 +35,14 @@ if __name__ == "__main__":
     exp_meta = ExpMeta("solo", 0, 0)
     dest_folder = get_data_folder(exp_meta)
 
-    # while x0 - x_ref > 0.01:
-    while x[0, 0] < track.length:
+    while ca.norm_2(x[0, 0] - track.length) > 1e-2:
         x_ref, curvature, s_0_arc, phi_0_arc = compute_ref_trajectory(
-            x=x, track=track, dt=model.dt, N=mpc.N, v_ref=0.5, e_ref=lane_width / 2
+            x=x, track=track, dt=model.dt, N=mpc.N, v_ref=0.5, e_ref=-lane_width / 2
         )
         uopt, xopt = mpc.get_ctrl(x, x_ref, curvature, s_0_arc, phi_0_arc)
         u = ca.vertcat(uopt, curvature[0], s_0_arc[0], phi_0_arc[0])
-        # State Feedback Loop
+        # State Feedback Step
         x = model.sim(1, u=u, input_noise=False, state_noise=False)
-        # states, inputs = model.get_trajectory()
-        vehicles = [VehicleData("example", COLORS["ego"], x, u)]  # states, inputs),
-        # print("s:", x[0, 0])
-        animate_trajectory(dest_folder, track, vehicles, "J0")
-
-    # states, inputs = model.get_trajectory()
-    # print(states, inputs)
+        states, inputs = model.get_trajectory()
+        vehicles = [VehicleData("example", COLORS["ego"], states, inputs)]
+        plot_trajectory(track, vehicles, plot_input=False)
