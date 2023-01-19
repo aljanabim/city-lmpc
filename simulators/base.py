b5241f177498f2e4dac7af124841c0e6fa7cdf9b
aljanabim <ma970101@yahoo.se>
2023-01-19 11:07:30 +0100

    Create an LMPC base simulator, use it, and add pre and post step

--- simulators/base.py
+++ simulators/base.py
@@ -1,15 +1,15 @@
 from typing import Tuple
-import casadi as ca
 from utils import sim_util, vis_util
-from models.solo import SoloFrenetModel
-from controllers.solo import SoloMPC
+from models.base import BaseModel
+from controllers.base import BaseMPC
 from models.track import Track
 
 
 class BaseSimulator:
     EXP_NAME = "base"
 
-    def __init__(self, model, controller, track):
+    # def __init__(self, model: BaseModel, controller: BaseMPC, track: Track):
+    def __init__(self, model, controller, track: Track):
         self.model = model
         self.controller = controller
         self.track = track
@@ -17,12 +17,13 @@ class BaseSimulator:
         self.x = self.model.x0
         self.exp_meta = sim_util.ExpMeta(self.EXP_NAME, 0, self.x[0, 0])
         self.vehicles: Tuple[vis_util.VehicleData] = None
+        self.time_step = 0
 
-    def keep_alive(self):
+    def keep_running(self):
         """
         Stop condition for the simulator. Returns False, when the simulator should stop.
         """
-        return True
+        return False
 
     def step(self):
         """
@@ -35,20 +36,42 @@ class BaseSimulator:
         """
         pass
 
+    def pre_step(self):
+        """
+        Sub-routines executed before the simulator does a step
+        """
+        pass
+
+    def post_step(self):
+        """
+        Sub-routines executed after the simulator does a step
+        """
+        self.time_step += 1
+
     def pre_run(self):
+        """
+        Sub-routines executed before the simulator starts running
+        """
         pass
 
     def post_run(self):
+        """
+        Sub-routines executed after the simulator starts running
+        """
         pass
 
     def run(self, plot=True):
         self.pre_run()
-        while self.keep_alive():
+        print("======= Running", self.__class__.__name__, "=======")
+        while self.keep_running():
+            self.pre_step()
             self.step()
+            self.post_step()
             if plot:
                 vis_util.plot_trajectory(
                     self.track, self.vehicles, plot_input=True)
         self.post_run()
+        print("=======", self.__class__.__name__, "Finished =======")
 
     def save(self, it=0):
         states, inputs = self.model.get_trajectory()
@@ -73,3 +96,24 @@ class BaseSimulator:
             animation_filename=f"{self.EXP_NAME}_J{it}",
             save=save
         )
+
+
+class BaseLMPCSimulator(BaseSimulator):
+    def __init__(self, model, controller, track: Track, iteration: int):
+        self.iteration = iteration
+        super().__init__(model, controller, track)
+
+    def keep_iterating(self):
+        return False
+
+    def pre_iteration(self):
+        pass
+
+    def post_iteration(self):
+        pass
+
+    def run(self, plot=True):
+        while self.keep_iterating():
+            print("Starting iteration", self.iteration)
+            super().run(plot)
+            print("Finished iteration", self.iteration)
--- simulators/solo.py
+++ simulators/solo.py
@@ -1,18 +1,22 @@
 import casadi as ca
 from utils import sim_util, vis_util
 from models.solo import SoloFrenetModel
-from controllers.solo import SoloMPC
+from controllers.solo import SoloMPC, SoloRelaxedLMPC
 from models.track import Track
-from simulators.base import BaseSimulator
+from simulators.base import BaseSimulator, BaseLMPCSimulator
 
 
 class SoloMPCSimulator(BaseSimulator):  # Using a Frenet Model
     EXP_NAME = "solo"
 
     def __init__(self, model: SoloFrenetModel, controller: SoloMPC, track: Track):
+        # Reassigned to make sure that the types are correct and not Any as in the BaseSimulator class
+        self.model = model
+        self.controller = controller
+        self.track = track
         super().__init__(model, controller, track)
 
-    def keep_alive(self):
+    def keep_running(self):
         return self.x[0, 0] < self.track.length
 
     def step(self):
@@ -21,6 +25,43 @@ class SoloMPCSimulator(BaseSimulator):  # Using a Frenet Model
         )
         uopt, xopt = self.controller.get_ctrl(
             self.x, x_ref, curvature, s_0_arc, phi_0_arc)
+        u = ca.vertcat(uopt, curvature[0], s_0_arc[0], phi_0_arc[0])
+        # State Feedback Step
+        self.x = self.model.sim(1, u=u, input_noise=False, state_noise=False)
+
+        states, inputs = self.model.get_trajectory()
+        self.vehicles = [vis_util.VehicleData(
+            "car", vis_util.COLORS["ego"], states, inputs)]
+
+
+class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
+    EXP_NAME = "solo"
+
+    def __init__(self, model: SoloFrenetModel, controller: SoloRelaxedLMPC, track: Track, iteration: int):
+        # Reassigned to make sure that the types are correct and not Any as in the BaseSimulator class
+        self.model = model
+        self.controller = controller
+        self.track = track
+        self.iteration = iteration
+        super().__init__(model, controller, track)
+
+    def keep_running(self):
+        return self.x[0, 0] < self.track.length
+
+    def get_stored_cost_to_go(self):
+        return ca.DM()
+
+    def get_stored_states(self):
+        return ca.DM()
+
+    def step(self):
+        x_ref, curvature, s_0_arc, phi_0_arc = sim_util.compute_ref_trajectory(
+            x=self.x, track=self.track, dt=self.model.dt, N=self.controller.N, v_ref=0.5, e_ref=self.model.x0[1, 0]
+        )
+        stored_cost_to_go = self.get_stored_cost_to_go()
+        stored_states = self.get_stored_states()
+        uopt, xopt = self.controller.get_ctrl(
+            self.x, curvature, s_0_arc, phi_0_arc, stored_cost_to_go, stored_states)
         u = ca.vertcat(uopt, curvature[0], s_0_arc[0], phi_0_arc[0])
         # State Feedback Step
         self.x = self.model.sim(1, u=u, input_noise=False, state_noise=False)
