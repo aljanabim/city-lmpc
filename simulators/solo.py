6a8a8936c6b6e3e179bda7ebf92e7de21e05cd8c
aljanabim <ma970101@yahoo.se>
2023-01-20 19:38:19 +0100

    Working scenario 1!!

--- simulators/solo.py
+++ simulators/solo.py
@@ -7,8 +7,6 @@ from simulators.base import BaseSimulator, BaseLMPCSimulator
 
 
 class SoloMPCSimulator(BaseSimulator):  # Using a Frenet Model
-    EXP_NAME = "solo"
-
     def __init__(self, model: SoloFrenetModel, controller: SoloMPC, track):
         # Override the types for model and controller (Any by default)
         self.model = model
@@ -24,7 +22,7 @@ class SoloMPCSimulator(BaseSimulator):  # Using a Frenet Model
             v_ref=0.5,
             e_ref=self.model.x0[1, 0],
         )
-        uopt, xopt = self.controller.get_ctrl(
+        uopt, _, _ = self.controller.get_ctrl(
             self.x, x_ref, curvature, s_0_arc, phi_0_arc
         )
         u = ca.vertcat(uopt, curvature[0], s_0_arc[0], phi_0_arc[0])
@@ -37,9 +35,7 @@ class SoloMPCSimulator(BaseSimulator):  # Using a Frenet Model
         ]
 
 
-class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
-    EXP_NAME = "solo"
-
+class SoloRelaxedLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
     def __init__(
         self,
         model: SoloFrenetModel,
@@ -53,9 +49,14 @@ class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
         self.controller = controller
         super().__init__(model, controller, track, trajectories, max_iter)
 
+    def reset(self):
+        super().reset()
+        self.slack_norm = ca.inf
+
     def compute_it_idx(self, iteration):
         T_opt = min(self.T.values())
-        return min(self.T[iteration] + self.time_step - T_opt, self.T[iteration])
+        # int to ensure it can be used as index
+        return int(min(self.T[iteration] + self.time_step - T_opt, self.T[iteration]))
 
     def get_stored_data(self):
         stored_cost_to_go = ca.DM()
@@ -64,10 +65,13 @@ class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
             zip(self.cost_to_go.values(), self.SSx.values())
         ):
             it_idx = self.compute_it_idx(j)
-            print(it_idx)
+            print("iteration", j, "k", it_idx)
             stored_cost_to_go = ca.horzcat(stored_cost_to_go, cost_to_go_j[:, it_idx:])
             stored_states = ca.horzcat(stored_states, states_j[:, it_idx:])
         return stored_cost_to_go, stored_states
+
+    def keep_running(self):
+        return self.slack_norm > 1e-8
 
     def step(self):
         _, curvature, s_0_arc, phi_0_arc = sim_util.compute_ref_trajectory(
@@ -84,7 +88,7 @@ class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
         self.controller.LAMBDA_SIZE = stored_cost_to_go.shape[1]
         self.controller.build_optimizer()
 
-        uopt, xopt = self.controller.get_ctrl(
+        uopt, _, _, self.slack_norm = self.controller.get_ctrl(
             self.x, curvature, s_0_arc, phi_0_arc, stored_cost_to_go, stored_states
         )
         u = ca.vertcat(uopt, curvature[0], s_0_arc[0], phi_0_arc[0])
@@ -95,3 +99,18 @@ class SoloLMPCSimulator(BaseLMPCSimulator):  # Using a Frenet Model
         self.vehicles = [
             vis_util.VehicleData("car", vis_util.COLORS["ego"], states, inputs)
         ]
+
+    def post_step(self):
+        super().post_step()
+        print(
+            "J-1 time",
+            self.cost_to_go[self.iteration - 1][0, 0],
+            "J time",
+            self.time_step,
+            "LAMBDA_SIZE",
+            self.controller.LAMBDA_SIZE,
+            "slack norm",
+            self.slack_norm,
+            "dist",
+            self.track.length - self.x[0, 0],
+        )
--- solo.py
+++ solo.py
@@ -2,10 +2,20 @@ import casadi as ca
 import numpy as np
 from controllers.solo import SoloMPC, SoloRelaxedLMPC
 from models.solo import SoloFrenetModel
-from simulators.solo import SoloMPCSimulator, SoloLMPCSimulator
-from utils import sim_util
+from simulators.solo import SoloMPCSimulator, SoloRelaxedLMPCSimulator
+from utils import sim_util, vis_util
+
 
 if __name__ == "__main__":
+    """
+    Totally works only weird thing is that the J1-11 vehicles don't go all the
+    way to xf and stop a little bit short, must be something related to relaxation
+    of constraints, but I'm not sure exactly what it is.
+
+    Tried to let the cost-to-go also include the full values with lambda, but got similar
+    results for J1 and then a bunch of bugs for further iterations.
+    """
+    EXP_NAME = "solo"
     lane_width = 0.5
     yaw0 = ca.pi
     track = sim_util.create_track(0, 0, yaw0)
@@ -15,28 +25,35 @@ if __name__ == "__main__":
     model = SoloFrenetModel(x)
 
     # Input and state constraints for MPC, +0.5 on s to allow reaching the target
-    xub = ca.vertcat(track.length + 0.5, lane_width, 1.5, ca.inf)
-    # uub = ca.vertcat(1, ca.pi / 5)
-    uub = ca.vertcat(1, ca.pi / 4)
+    xub = ca.vertcat(track.length + 0.5, lane_width - model.WB / 2, 1.5, ca.inf)
+    uub = ca.vertcat(0.75, ca.pi / 4)
 
     # Get trajectory for initial iteration
     mpc = SoloMPC(
         model,
         Q=ca.diag((1, 300, 200, 20)),
-        R=ca.diag((5, 4)),
+        R=ca.diag((100, 4)),
         xlb=-xub,
         xub=xub,
         ulb=-uub,
         uub=uub,
     )
     simulator = SoloMPCSimulator(model, mpc, track)
-    simulator.EXP_NAME = "solo"
+    simulator.EXP_NAME = EXP_NAME
     traj_0 = simulator.load(iteration=0)
     if traj_0 is None:  # if J0 is not stored, run simulator, save, then load it
         traj_0 = simulator.run().save().load()
 
-    # # Start LMPC learning
+    print(traj_0["states"].shape)  # => (4, 317) => T0 = 316
+    # track.length => 10.587873909802939
+    # traj_0["states"][0, -1] => 10.59435249
+
+    vis_util.compare_iterations(EXP_NAME, 0, 11, track, step=1)
+    load_until = 11
+    trajectories = [simulator.load(j) for j in range(0, load_until + 1)]
+
+    # # # Start LMPC learning
     lmpc = SoloRelaxedLMPC(model, Q=None, R=None, xlb=-xub, xub=xub, ulb=-uub, uub=uub)
-    simulator = SoloLMPCSimulator(model, lmpc, track, [traj_0], 10)
-    simulator.EXP_NAME = "solo"
-    simulator.run()
+    simulator = SoloRelaxedLMPCSimulator(model, lmpc, track, trajectories, max_iter=15)
+    simulator.EXP_NAME = EXP_NAME
+    # simulator.run()
