c18c26a34766a6b22e4884e0de36a9dc8f0940b9
aljanabim <ma970101@yahoo.se>
2023-01-25 15:07:16 +0100

    Include ObstacleMPC (instead of using SoloMPC

--- controllers/obstacle.py
+++ controllers/obstacle.py
@@ -4,13 +4,62 @@ from controllers.base import BaseMPC
 # For iteration 0 we use SoloMPC in this scenario too
 
 
+class ObstacleMPC(BaseMPC):
+    # Uses the SoloFrenetModel
+    def set_parameters(self):
+        self.x_ref = self.opti.parameter(self.n_states, self.N + 1)
+        self.curvature = self.opti.parameter(1, self.N)
+        self.s_0_arc = self.opti.parameter(1, self.N)
+        self.phi_0_arc = self.opti.parameter(1, self.N)
+        return self.curvature, self.s_0_arc, self.phi_0_arc
+
+    def set_cost(self):
+        self.cost = 0
+
+        n_sub = self.n_states - 1  # number of states ignore the yaw
+        for i in range(self.N):
+            err = self.x[:n_sub, i] - self.x_ref[:n_sub, i]
+            self.cost += (
+                err.T
+                @ self.Q[:n_sub, :n_sub]
+                @ err
+                # + self.u[:, i].T @ self.R @ self.u[:, i]
+            )
+            # Calculate the angle difference towards the smallest angle error
+            # from https://math.stackexchange.com/questions/341749/how-to-get-the-minimum-angle-between-two-crossing-lines
+            angle_err = ca.pi - ca.norm_2(
+                ca.norm_2(self.x[-1, i] - self.x_ref[-1, i]) - ca.pi
+            )
+            self.cost += angle_err**2 * self.Q[-1, -1]
+
+        errN = self.x[:n_sub, self.N] - self.x_ref[:n_sub, self.N]
+        self.cost += errN.T @ self.Q[:n_sub, :n_sub] @ errN
+        angle_err = ca.pi - ca.norm_2(
+            ca.norm_2(self.x[-1, self.N] - self.x_ref[-1, self.N]) - ca.pi
+        )
+        self.cost += angle_err**2 * self.Q[-1, -1]
+
+        if self.R is not None:
+            for t in range(self.N - 1):
+                u_diff = self.u[:, t + 1] - self.u[:, t]
+                self.cost += u_diff.T @ self.R @ u_diff
+
+    def get_ctrl(self, x0, x_ref, curvature, s_0_arc, phi_0_arc):
+        self.opti.set_value(self.x0, x0)
+        self.opti.set_value(self.x_ref, x_ref)
+        self.opti.set_value(self.curvature, curvature)
+        self.opti.set_value(self.s_0_arc, s_0_arc)
+        self.opti.set_value(self.phi_0_arc, phi_0_arc)
+        return self.solve()
+
+
 class ObstacleLMPC(BaseMPC):
     # Uses the SoloFrenetModel
 
     def set_variables(self):
         self.x = self.opti.variable(self.n_states, self.N + 1)
         self.u = self.opti.variable(self.n_inputs, self.N)  # For multi-shooting
-        # self.terminal_state_slack = self.opti.variable(self.n_states, 1)
+        self.terminal_state_slack = self.opti.variable(self.n_states, 1)
 
     def set_parameters(self):
         self.curvature = self.opti.parameter(1, self.N)
@@ -29,21 +78,32 @@ class ObstacleLMPC(BaseMPC):
             s_err = self.x[0, t] - self.s_final
             # Use algebraic sigmoid to define a smooth cost function=11
             self.cost += (1 / 2) * (k * s_err / ca.sqrt(1 + (k * s_err) ** 2) + 1)
+        # Input cost
+        # if self.R is not None:
+        #     for t in range(self.N):
+        #         self.cost += self.u[:, t].T @ self.R @ self.u[:, t]
+        if self.R is not None:
+            for t in range(self.N - 1):
+                u_diff = self.u[:, t + 1] - self.u[:, t]
+                self.cost += u_diff.T @ self.R @ u_diff
+
+        # terminal cost
         self.cost += self.terminal_cost
 
         # self.cost = self.N + self.terminal_cost
-        # + 1e2 * (self.terminal_state_slack.T @ self.terminal_state_slack)
+        self.cost += 1e3 * (self.terminal_state_slack.T @ self.terminal_state_slack)
 
     def set_nonlinear_constraints(self):
         # Terminal state constraint
         # self.opti.subject_to(self.x[:, self.N] == self.terminal_state)
         self.opti.subject_to(
-            # self.terminal_state_slack +
+            self.terminal_state_slack +
+            #
             self.x[:, self.N]
             == self.terminal_state
         )
         # Collision avoidance with rect ellipse
-        deg = 4
+        deg = 2
         L = self.model.LENGTH
         W = self.model.WIDTH
         dL = (2 ** (1 / deg) - 1) * L
@@ -83,8 +143,7 @@ class ObstacleLMPC(BaseMPC):
         self.opti.set_value(self.s_obs, s_obs)
         self.opti.set_value(self.s_final, s_final)
         u_pred, x_pred, cost = self.solve(show_infeasibilities=False)
-        # cost_without_slack = self.N + terminal_cost
-        # slack_value = self.opti.value(self.terminal_state_slack)
-        # slack_norm = slack_value.T @ slack_value
-        slack_norm = 0
+        slack_value = self.opti.value(self.terminal_state_slack)
+        slack_norm = slack_value.T @ slack_value
+        # slack_norm = 0
         return u_pred, x_pred, cost, slack_norm
--- obstacle.py
+++ obstacle.py
@@ -1,6 +1,6 @@
 import casadi as ca
 import numpy as np
-from controllers.obstacle import ObstacleLMPC
+from controllers.obstacle import ObstacleLMPC, ObstacleMPC
 from simulators.solo import SoloMPCSimulator, SoloRelaxedLMPCSimulator
 from simulators.obstacle import ObstacleLMPCSimulator, ObstacleMPCSimulator
 from utils import sim_util, vis_util
@@ -9,7 +9,7 @@ from utils import sim_util, vis_util
 if __name__ == "__main__":
     EXP_NAME = "obstacle"
     # use same model track and solo as in the solo scenario
-    model, mpc, track, track_J0 = sim_util.setup_solo()
+    model, mpc, track, track_J0 = sim_util.setup_solo(ObstacleMPC, R=ca.diag((10, 50)))
     s_obs = track.length / 2
     simulator = ObstacleMPCSimulator(model, mpc, track_J0)
     simulator.EXP_NAME = EXP_NAME
@@ -19,11 +19,7 @@ if __name__ == "__main__":
     if traj_0 is None:  # if J0 is not stored, run simulator, save, then load it
         traj_0 = simulator.run().save().load()
 
-    # print(traj_0["states"].shape)  # => (4, 317) => T0 = 316
-    # # track.length => 10.587873909802939
-    # # traj_0["states"][0, -1] => 10.59435249
-
-    # vis_util.compare_iterations(EXP_NAME, 0, 2, track, step=1, s_obs=s_obs)
+    # vis_util.compare_iterations(EXP_NAME, 0, 0, track, step=1, s_obs=s_obs)
     # quit()
     load_until = 0
     trajectories = [simulator.load(j) for j in range(0, load_until + 1)]
@@ -38,8 +34,12 @@ if __name__ == "__main__":
     # model.set_x0(x0)
     # time_step = np.argmin(np.abs(traj_0["states"][0, :] - x0[0, 0]))
 
+    # TODO mention in report that 0.1 for v_u == penalize acceleration
+    # turning is more expensive
+    # Kepp u_diff i LMPC cost, because of min time, we just want smoother control
+    R = ca.diag((10, 50))
     lmpc = ObstacleLMPC(
-        model, Q=None, R=None, xlb=-mpc.xub, xub=mpc.xub, ulb=-mpc.uub, uub=mpc.uub
+        model, Q=None, R=R, xlb=-mpc.xub, xub=mpc.xub, ulb=-mpc.uub, uub=mpc.uub
     )
     # Use track not track_J0 in simulator because it has a shorter end (symmetric)
     simulator = ObstacleLMPCSimulator(model, lmpc, track, trajectories, max_iter=10)
