f022d2e7dc5f15b0cd7e6a48b19948c13d19429f
aljanabim <ma970101@yahoo.se>
2023-01-27 22:05:28 +0100

    Support control rate constraints in MPC and adopt algebraic sigmoid and u_prev in LMPC

--- controllers/solo.py
+++ controllers/solo.py
@@ -9,6 +9,7 @@ class SoloMPC(BaseMPC):
         self.curvature = self.opti.parameter(1, self.N)
         self.s_0_arc = self.opti.parameter(1, self.N)
         self.phi_0_arc = self.opti.parameter(1, self.N)
+        self.u_prev = self.opti.parameter(self.n_inputs, 1)
         return self.curvature, self.s_0_arc, self.phi_0_arc
 
     def set_cost(self):
@@ -35,18 +36,29 @@ class SoloMPC(BaseMPC):
         )
         self.cost += angle_err**2 * self.Q[-1, -1]
 
-    def get_ctrl(self, x0, x_ref, curvature, s_0_arc, phi_0_arc):
+    def set_linear_constraints(self):
+        # Set control rate constraints
+        uub_rate = self.model.dt * ca.DM([[1], [0.7]])
+        u_rate = ca.sqrt((self.u[:, :-1] - self.u[:, 1:]) ** 2)
+        u_rate = ca.horzcat(ca.sqrt((self.u_prev - self.u[:, 0]) ** 2), u_rate)
+        self.opti.subject_to(self.opti.bounded(-uub_rate, u_rate, uub_rate))
+        # Set control and state constraints
+        return super().set_linear_constraints()
+
+    def get_ctrl(self, x0, x_ref, curvature, s_0_arc, phi_0_arc, u_prev):
         self.opti.set_value(self.x0, x0)
         self.opti.set_value(self.x_ref, x_ref)
         self.opti.set_value(self.curvature, curvature)
         self.opti.set_value(self.s_0_arc, s_0_arc)
         self.opti.set_value(self.phi_0_arc, phi_0_arc)
+        self.opti.set_value(self.u_prev, u_prev)
         return self.solve()
 
 
-class SoloRelaxedLMPC(BaseMPC):
+class SoloRelaxedLMPC(SoloMPC):
     # Uses the SoloFrenetModel
     LAMBDA_SIZE = 10
+    L = 20
 
     def set_variables(self):
         self.x = self.opti.variable(self.n_states, self.N + 1)
@@ -58,17 +70,21 @@ class SoloRelaxedLMPC(BaseMPC):
         self.curvature = self.opti.parameter(1, self.N)
         self.s_0_arc = self.opti.parameter(1, self.N)
         self.phi_0_arc = self.opti.parameter(1, self.N)
-        # self.terminal_cost = self.opti.parameter(1, 1)
-        # self.terminal_state = self.opti.parameter(self.n_states, 1)
         self.stored_cost_to_go = self.opti.parameter(1, self.LAMBDA_SIZE)
         self.stored_states = self.opti.parameter(self.n_states, self.LAMBDA_SIZE)
+        self.s_final = self.opti.parameter(1, 1)
+        self.u_prev = self.opti.parameter(self.n_inputs, 1)
         return self.curvature, self.s_0_arc, self.phi_0_arc
 
     def set_cost(self):
-        self.cost = (
-            self.N
-            + self.lam @ self.stored_cost_to_go.T
-            + 1e2 * (self.lam_slack.T @ self.lam_slack)
+        self.cost = 0
+        k = -4
+        for t in range(self.N):
+            s_err = self.x[0, t] - self.s_final
+            # Use algebraic sigmoid to define a smooth cost function=11
+            self.cost += (1 / 2) * (k * s_err / ca.sqrt(1 + (k * s_err) ** 2) + 1)
+        self.cost = self.lam @ self.stored_cost_to_go.T + self.L * (
+            self.lam_slack.T @ self.lam_slack
         )
 
     def set_nonlinear_constraints(self):
@@ -82,7 +98,15 @@ class SoloRelaxedLMPC(BaseMPC):
         )
 
     def get_ctrl(
-        self, x0, curvature, s_0_arc, phi_0_arc, stored_cost_to_go, stored_states
+        self,
+        x0,
+        curvature,
+        s_0_arc,
+        phi_0_arc,
+        stored_cost_to_go,
+        stored_states,
+        s_final,
+        u_prev,
     ):
         self.opti.set_value(self.x0, x0)
         self.opti.set_value(self.curvature, curvature)
@@ -90,6 +114,8 @@ class SoloRelaxedLMPC(BaseMPC):
         self.opti.set_value(self.phi_0_arc, phi_0_arc)
         self.opti.set_value(self.stored_cost_to_go, stored_cost_to_go)
         self.opti.set_value(self.stored_states, stored_states)
+        self.opti.set_value(self.s_final, s_final)
+        self.opti.set_value(self.u_prev, u_prev)
         u_pred, x_pred, cost = self.solve()
         lambda_slack = self.opti.value(self.lam_slack)
         slack_norm = lambda_slack.T @ lambda_slack
